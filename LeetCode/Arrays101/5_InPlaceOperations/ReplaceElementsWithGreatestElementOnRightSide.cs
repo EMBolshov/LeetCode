470faaa2e31bf9ffb7ce0dad4e4a69fb26670ca7
Evgeniy Bolshov <e.m.bolshov@gmail.com>
2021-02-11 23:12:42 +0300

    Arrays101: In-place operations

---
+++ LeetCode/Arrays101/5_InPlaceOperations/ReplaceElementsWithGreatestElementOnRightSide.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+
+namespace LeetCode.Arrays101._5_InPlaceOperations
+{
+    /// <summary>
+    /// https://leetcode.com/explore/learn/card/fun-with-arrays/511/in-place-operations/3259/
+    /// </summary>
+    public static class ReplaceElementsWithGreatestElementOnRightSide
+    {
+        public static int[] ReplaceElements(int[] arr)
+        {
+            var res = new int[arr.Length];
+            var max = arr[^1];
+
+            for (int i = arr.Length - 2; i >= 0; i--)
+            {
+                res[i] = max;
+                
+                if (arr[i] > max)
+                    max = arr[i];
+            }
+
+            res[^1] = -1;
+            return res;            
+        }
+
+        // Time Limit Exceeded. :(
+        private static int GetMax(int[] arr, int n)
+        {
+            return arr.Skip(n).Max();
+        }
+    }
+}
---
+++ TestSolutions/Array101/5_InPlaceOperations/CheckSolution.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using FluentAssertions;
+using LeetCode.Arrays101._5_InPlaceOperations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestSolutions.Array101._5_InPlaceOperations
+{
+    [TestClass]
+    public class CheckSolution
+    {
+        [TestMethod]
+        public void ReplaceElementsTest()
+        {
+            var input = new[] {17, 18, 5, 4, 6, 1};
+            var output = ReplaceElementsWithGreatestElementOnRightSide.ReplaceElements(input);
+            output.SequenceEqual(new[] {18, 6, 6, 6, 1, -1}).Should().BeTrue();
+
+            input = new[] {400};
+            output = ReplaceElementsWithGreatestElementOnRightSide.ReplaceElements(input);
+            output.SequenceEqual(new[] {-1}).Should().BeTrue();
+        }
+    }
+}
