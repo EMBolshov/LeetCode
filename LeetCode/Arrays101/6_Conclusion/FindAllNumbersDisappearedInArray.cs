b331963d324615d4adf555d29ef0cf566443bb6c
Evgeniy Bolshov <e.m.bolshov@gmail.com>
2021-03-02 21:48:17 +0300

    Arrays101: Conclusion - Find all numbers disappeared in array

--- LeetCode/Arrays101/6_Conclusion/FindAllNumbersDisappearedInArray.cs
+++ LeetCode/Arrays101/6_Conclusion/FindAllNumbersDisappearedInArray.cs
@@ -6,11 +6,58 @@ namespace LeetCode.Arrays101._6_Conclusion
     /// <summary>
     /// https://leetcode.com/explore/learn/card/fun-with-arrays/523/conclusion/3270/
     /// </summary>
-    public class FindAllNumbersDisappearedInArray
+    public static class FindAllNumbersDisappearedInArray
     {
-        public IList<int> FindDisappearedNumbers(int[] nums)
+        public static IList<int> FindDisappearedNumbers(int[] nums)
         {
-            throw new NotImplementedException();
+            var res = new List<int>();
+            
+            if (nums.Length == 0)
+                return res;
+            
+            Array.Sort(nums);
+            var prev = nums[0];
+
+            var ptr = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                ptr++;
+                
+                if (nums[i] == prev && i > 0 && i + 1 < nums.Length)
+                {
+                    ++i;
+                }
+                
+                if (ptr != nums[i] && ptr <= nums.Length)
+                {
+                    res.Add(ptr);
+                    --i;
+                }
+                else
+                    prev = nums[i];
+                
+            }
+
+            if (nums[^1] != nums.Length && !res.Contains(nums.Length))
+                res.Add(nums.Length);
+
+            return res;
+        }
+
+        private static IList<int> BadSolution(int[] nums)
+        {
+            var res = new List<int>();
+            for (int i = 1; i < nums.Length + 1; i++)
+                res.Add(i);
+
+            foreach (var t in nums)
+            {
+                if (res.Contains(t))
+                    res.Remove(t);
+            }
+
+            return res;
         }
     }
 }
