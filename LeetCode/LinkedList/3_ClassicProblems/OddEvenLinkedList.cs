a4c3f26b501159f61145325bd0f91ac49358a32d
Evgeniy Bolshov <e.m.bolshov@gmail.com>
2021-03-18 20:39:40 +0300

    Linked List - Odd Even Linked List

---
+++ LeetCode/LinkedList/3_ClassicProblems/OddEvenLinkedList.cs
@@ -0,0 +1,48 @@
+﻿using LeetCode.LinkedList._1_SinglyLinkedList;
+
+namespace LeetCode.LinkedList._3_ClassicProblems
+{
+    /// <summary>
+    /// https://leetcode.com/explore/learn/card/linked-list/219/classic-problems/1208/
+    /// </summary>
+    public static class OddEvenLinkedList
+    {
+        public static MyLinkedList OddEvenList(MyLinkedList head)
+        {
+            if (head == null)
+                return null;
+            
+            var result = head;
+            var evenFirst = head.Next;
+
+            while (head != null)
+            {
+                var even = head.Next;
+                
+                head.Next = head.Next?.Next;
+
+                if (even != null)
+                {
+                    even.Next = even.Next?.Next;
+                }
+                else
+                {
+                    head.Next = evenFirst;
+                    head = null;
+                    continue;
+                }
+
+                head.Next ??= evenFirst;
+                if (head.Next == evenFirst)
+                {
+                    head = null;
+                    continue;
+                }
+                
+                head = head.Next;
+            }
+
+            return result;
+        }
+    }
+}
--- TestSolutions/LinkedList/3_ClassicProblems/CheckSolution.cs
+++ TestSolutions/LinkedList/3_ClassicProblems/CheckSolution.cs
@@ -39,5 +39,21 @@ namespace TestSolutions.LinkedList._3_ClassicProblems
             });
 
         }
+        
+        [TestMethod]
+        public void OddEvenListTest()
+        {
+            var list = CommonHelpers.GetLinkedListFromArray(new[] {1, 2, 3, 4, 5});
+            OddEvenLinkedList.OddEvenList(list).ValuesToFlatList().Should().BeEquivalentTo(new List<int>
+            {
+                1, 3, 5, 2, 4
+            });
+            
+            list = CommonHelpers.GetLinkedListFromArray(new[] {1, 2, 3, 4, 5, 6, 7, 8});
+            OddEvenLinkedList.OddEvenList(list).ValuesToFlatList().Should().BeEquivalentTo(new List<int>
+            {
+                1, 3, 5, 7, 2, 4, 6, 8
+            });
+        }
     }
 }
