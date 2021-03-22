5f1d91a4fc487eba4e9728fc22f75ac66ab9ee0c
Evgeniy Bolshov <e.m.bolshov@gmail.com>
2021-03-22 11:51:20 +0300

    Binary Tree - binary tree inorder traversal iteratively (using stack and hashset)

---
+++ LeetCode/BinaryTree/1_TraverseATree/BinaryTreeInorderTraversal.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using LeetCode.BinaryTree._0_BinaryTree;
+
+namespace LeetCode.BinaryTree._1_TraverseATree
+{
+    /// <summary>
+    /// https://leetcode.com/explore/learn/card/data-structure-tree/134/traverse-a-tree/929/
+    /// </summary>
+    public static class BinaryTreeInorderTraversal
+    {
+        public static IList<int> InorderTraversal(MyBinaryTree tree)
+        {
+            var result = new List<int>();
+
+            var processed = new HashSet<MyBinaryTree>();
+            var depthStack = new Stack<MyBinaryTree>();
+
+            if (tree == null)
+                return result;
+
+            var root = tree;
+
+            while (root != null)
+            {
+                depthStack.Push(root);
+
+                if (root.Left != null)
+                {
+                    root = root.Left;
+                }
+                else if (root.Right != null)
+                {
+                    AddIfNotContains(root);
+                    
+                    depthStack.Pop();
+                    root = root.Right;
+                }
+                else
+                {
+                    if (depthStack.Any() && depthStack.Peek() == root)
+                    {
+                        AddIfNotContains(root);
+                        
+                        depthStack.Pop();
+
+                        if (depthStack.TryPeek(out var node))
+                        {
+                            AddIfNotContains(node);
+                        }
+
+                        var complete = false;
+                        while (!complete)
+                        {
+                            if (depthStack.TryPeek(out node))
+                            {
+                                if (node.Right == null)
+                                {
+                                    AddIfNotContains(node);
+                                    depthStack.Pop();
+                                }
+                                else if (processed.Contains(node.Right))
+                                {
+                                    depthStack.Pop();
+                                }
+                                else
+                                {
+                                    complete = true;
+                                    AddIfNotContains(node);
+                                }
+                            }
+                            else
+                            {
+                                complete = true;
+                            }
+                        }
+                    }
+
+                    if (depthStack.Any())
+                    {
+                        root = depthStack.Peek().Right;
+                    }
+                    else
+                    {
+                        root = null;
+                    }
+                }
+            }
+
+            return result;
+            
+            void AddIfNotContains(MyBinaryTree node)
+            {
+                if (!processed.Contains(node))
+                {
+                    result.Add(node.Value);
+                    processed.Add(node);
+                }
+            }
+        }
+    }
+}
--- TestSolutions/BinaryTree/1_TraverseATree/CheckSolution.cs
+++ TestSolutions/BinaryTree/1_TraverseATree/CheckSolution.cs
@@ -41,5 +41,62 @@ namespace TestSolutions.BinaryTree._1_TraverseATree
                 1, 4, 2, 3
             });
         }
+        
+        [TestMethod]
+        public void InorderTraversalTestCase1()
+        {
+            var tree = new MyBinaryTree(6);
+            tree.Left = new MyBinaryTree(2);
+            tree.Left.Left = new MyBinaryTree(1);
+            tree.Left.Right = new MyBinaryTree(4);
+            tree.Left.Right.Left = new MyBinaryTree(3);
+            tree.Left.Right.Right = new MyBinaryTree(5);
+            tree.Right = new MyBinaryTree(7);
+            tree.Right.Right = new MyBinaryTree(8);
+            tree.Right.Right.Left = new MyBinaryTree(9);
+
+            BinaryTreeInorderTraversal.InorderTraversal(tree).Should().BeEquivalentTo(new List<int>
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9
+            });
+        }
+        
+        [TestMethod]
+        public void InorderTraversalSingleNodeTest()
+        {
+            var tree = new MyBinaryTree(1);
+            
+            BinaryTreeInorderTraversal.InorderTraversal(tree).Should().BeEquivalentTo(new List<int>
+            {
+                1
+            });
+        }
+        
+        [TestMethod]
+        public void InorderTraversalTestCase2()
+        {
+            var tree = new MyBinaryTree(2);
+            tree.Left = new MyBinaryTree(3);
+            tree.Left.Left = new MyBinaryTree(1);
+
+            BinaryTreeInorderTraversal.InorderTraversal(tree).Should().BeEquivalentTo(new List<int>
+            {
+                1, 3, 2
+            });
+        }
+        
+        [TestMethod]
+        public void InorderTraversalTestCase3()
+        {
+            var tree = new MyBinaryTree(4);
+            tree.Left = new MyBinaryTree(1);
+            tree.Left.Left = new MyBinaryTree(2);
+            tree.Left.Left.Left = new MyBinaryTree(3);
+
+            BinaryTreeInorderTraversal.InorderTraversal(tree).Should().BeEquivalentTo(new List<int>
+            {
+                3, 2, 1, 4
+            });
+        }
     }
 }
