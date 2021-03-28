729512e534b4485b5c6886261991a928c04df012
Evgeniy Bolshov <e.m.bolshov@gmail.com>
2021-03-28 18:47:30 +0300

    Binary Tree - binary tree level order traversal iteratively (using queue)

---
+++ LeetCode/BinaryTree/1_TraverseATree/BinaryTreeLevelOrderTraversal.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LeetCode.BinaryTree._0_BinaryTree;
+
+namespace LeetCode.BinaryTree._1_TraverseATree
+{
+    public static class BinaryTreeLevelOrderTraversal
+    {
+        public static IList<IList<int>> LevelOrderTraversal(MyBinaryTree tree)
+        {
+            var result = new List<IList<int>>();
+            
+            if (tree == null)
+                return result;
+            
+            var queue = new Queue<MyBinaryTree>();
+            var root = tree;
+            
+            result.Add(new List<int>{root.Value});
+            queue.Enqueue(root.Left);
+            queue.Enqueue(root.Right);
+
+            while (true)
+            {
+                var newQueue = new Queue<MyBinaryTree>();
+                var currentLevel = new List<int>();
+
+                while (queue.Any())
+                {
+                    var node = queue.Dequeue();
+                    if (node != null)
+                    {
+                        currentLevel.Add(node.Value);
+
+                        newQueue.Enqueue(node.Left);
+                        newQueue.Enqueue(node.Right);
+                    }
+                }
+                
+                if (currentLevel.Any())
+                    result.Add(currentLevel);
+
+                if (newQueue.Any())
+                {
+                    queue = newQueue;
+                    continue;
+                }
+
+                break;
+            }
+
+            return result;
+        }
+    }
+}
--- TestSolutions/BinaryTree/1_TraverseATree/CheckSolution.cs
+++ TestSolutions/BinaryTree/1_TraverseATree/CheckSolution.cs
@@ -21,7 +21,7 @@ namespace TestSolutions.BinaryTree._1_TraverseATree
             tree.Right = new MyBinaryTree(7);
             tree.Right.Right = new MyBinaryTree(9);
             tree.Right.Right.Left = new MyBinaryTree(8);
-            
+
             BinaryTreePreorderTraversal.PreorderTraversal(tree).Should().ContainInOrder(new List<int>
             {
                 6, 2, 1, 4, 3, 5, 7, 9, 8
@@ -143,6 +143,59 @@ namespace TestSolutions.BinaryTree._1_TraverseATree
             {
                 4, 3, 2, 1
             });
+        }
+
+        [TestMethod]
+        public void LevelOrderTraversalTestCase1()
+        {
+            var tree = new MyBinaryTree(6);
+            tree.Left = new MyBinaryTree(2);
+            tree.Left.Left = new MyBinaryTree(1);
+            tree.Left.Right = new MyBinaryTree(4);
+            tree.Left.Right.Left = new MyBinaryTree(3);
+            tree.Left.Right.Right = new MyBinaryTree(5);
+            tree.Right = new MyBinaryTree(7);
+            tree.Right.Right = new MyBinaryTree(9);
+            tree.Right.Right.Left = new MyBinaryTree(8);
+
+            var result = BinaryTreeLevelOrderTraversal.LevelOrderTraversal(tree);
+            result[0].Should().ContainInOrder(new List<int> {6});
+            result[1].Should().ContainInOrder(new List<int> {2, 7});
+            result[2].Should().ContainInOrder(new List<int> {1, 4, 9});
+            result[3].Should().ContainInOrder(new List<int> {3, 5, 8});
+        }
+        
+        [TestMethod]
+        public void LevelOrderTraversalTestCase2()
+        {
+            var tree = new MyBinaryTree(1);
+            tree.Left = new MyBinaryTree(2);
+            tree.Left.Left = new MyBinaryTree(3);
+            tree.Left.Left.Left = new MyBinaryTree(4);
+            tree.Left.Left.Left.Left = new MyBinaryTree(5);
+            
+
+            var result = BinaryTreeLevelOrderTraversal.LevelOrderTraversal(tree);
+            result[0].Should().ContainInOrder(new List<int> {1});
+            result[1].Should().ContainInOrder(new List<int> {2});
+            result[2].Should().ContainInOrder(new List<int> {3});
+            result[3].Should().ContainInOrder(new List<int> {4});
+            result[4].Should().ContainInOrder(new List<int> {5});
+        }
+        
+        [TestMethod]
+        public void LevelOrderTraversalTestCase3()
+        {
+            var tree = new MyBinaryTree(3);
+            tree.Left = new MyBinaryTree(9);
+            tree.Right = new MyBinaryTree(20);
+            tree.Right.Left = new MyBinaryTree(15);
+            tree.Right.Right = new MyBinaryTree(7);
+
+            var result = BinaryTreeLevelOrderTraversal.LevelOrderTraversal(tree);
+            result[0].Should().ContainInOrder(new List<int> {3});
+            result[1].Should().ContainInOrder(new List<int> {9, 20});
+            result[2].Should().ContainInOrder(new List<int> {15, 7});
         }
     }
 }
