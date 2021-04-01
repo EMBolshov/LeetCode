3e769909080ae64c7812d632e439fa03dff52b5e
Evgeniy Bolshov <e.m.bolshov@gmail.com>
2021-04-01 11:10:03 +0300

    Binary Tree - path sum

---
+++ LeetCode/BinaryTree/2_SolveProblemsRecursively/PathSum.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode.BinaryTree._0_BinaryTree;
+
+namespace LeetCode.BinaryTree._2_SolveProblemsRecursively
+{
+    /// <summary>
+    /// https://leetcode.com/explore/learn/card/data-structure-tree/17/solve-problems-recursively/537/
+    /// </summary>
+    public static class PathSum
+    {
+        public static bool HasPathSum(MyBinaryTree root, int targetSum)
+        {
+            return root != null && HasPathSumRecursively(root, 0, targetSum);
+
+            bool HasPathSumRecursively(MyBinaryTree root, int current, int target)
+            {
+                var complete = false;
+                current += root.Value;
+
+                if (root.Left == null && root.Right == null && current == target)
+                    return true;
+
+                if (root.Left != null)
+                    complete = HasPathSumRecursively(root.Left, current, target);
+
+                if (root.Right != null)
+                    complete = complete || HasPathSumRecursively(root.Right, current, target);
+
+                return complete;
+            }
+        }
+    }
+}
--- TestSolutions/BinaryTree/2_SolveProblemsRecursively/CheckSolution.cs
+++ TestSolutions/BinaryTree/2_SolveProblemsRecursively/CheckSolution.cs
@@ -188,5 +188,40 @@ namespace TestSolutions.BinaryTree._2_SolveProblemsRecursively
             
             SymmetricTree.IsSymmetric(tree).Should().Be(false);
         }
+        
+        [TestMethod]
+        public void PathSumTestCase1()
+        {
+            var tree = new MyBinaryTree(5);
+            tree.Left = new MyBinaryTree(4);
+            tree.Left.Left = new MyBinaryTree(11);
+            tree.Left.Left.Left = new MyBinaryTree(7);
+            tree.Left.Left.Right = new MyBinaryTree(2);
+            tree.Right = new MyBinaryTree(8);
+            tree.Right.Left = new MyBinaryTree(4);
+            tree.Right.Left.Left = new MyBinaryTree(1);
+            tree.Right.Right = new MyBinaryTree(13);
+            
+            PathSum.HasPathSum(tree, 22).Should().Be(true);
+        }
+        
+        [TestMethod]
+        public void PathSumTestCase2()
+        {
+            var tree = new MyBinaryTree(1);
+            tree.Left = new MyBinaryTree(2);
+            tree.Right = new MyBinaryTree(3);
+            
+            PathSum.HasPathSum(tree, 5).Should().Be(false);
+        }
+        
+        [TestMethod]
+        public void PathSumTestCase3()
+        {
+            var tree = new MyBinaryTree(1);
+            tree.Left = new MyBinaryTree(2);
+            
+            PathSum.HasPathSum(tree,1).Should().Be(false);
+        }
     }
 }
