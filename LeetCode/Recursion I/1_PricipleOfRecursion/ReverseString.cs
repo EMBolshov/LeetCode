a211c222de2df97d000d591582df164d2abe9815
Evgeniy Bolshov <e.m.bolshov@gmail.com>
2021-04-03 13:27:04 +0300

    Recursion - reverse string

---
+++ LeetCode/Recursion I/1_PricipleOfRecursion/ReverseString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Recursion_I._1_PricipleOfRecursion
+{
+    /// <summary>
+    /// https://leetcode.com/explore/learn/card/recursion-i/250/principle-of-recursion/1440/
+    /// </summary>
+    public static class ReverseString
+    {
+        public static void StringReverse(char[] s)
+        {
+            Reverse(s, 0, s.Length - 1);
+
+            void Reverse(char[] s, int left, int right)
+            {
+                if (left > right)
+                    return;
+                
+                var temp = s[left];
+                s[left] = s[right];
+                s[right] = temp;
+                
+                Reverse(s, left + 1, right - 1);
+            }
+        }
+    }
+}
---
+++ TestSolutions/Recursion I/1_PrincipleOfRecursion/CheckSolution.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using FluentAssertions;
+using LeetCode.Recursion_I._1_PricipleOfRecursion;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestSolutions.Recursion_I._1_PrincipleOfRecursion
+{
+    [TestClass]
+    public class CheckSolution
+    {
+        [TestMethod]
+        public void ReverseStringTest()
+        {
+            var input = new[] {'h', 'e', 'l', 'l', 'o'};
+            ReverseString.StringReverse(input);
+            input.SequenceEqual(new[] {'o', 'l', 'l', 'e', 'h'}).Should().BeTrue();
+            
+            input = new[] {'H', 'a', 'n', 'n', 'a', 'h'};
+            ReverseString.StringReverse(input);
+            input.SequenceEqual(new[] {'h', 'a', 'n', 'n', 'a', 'H'}).Should().BeTrue();
+        }
+    }
+}
