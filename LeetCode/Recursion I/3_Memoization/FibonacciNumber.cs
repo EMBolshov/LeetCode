7bf1566feb8a70bbb0c6f8080bdd5a0db0b320c6
Evgeniy Bolshov <e.m.bolshov@gmail.com>
2021-04-05 14:44:00 +0300

    Recursion - Fibonacci numbers with memoization

---
+++ LeetCode/Recursion I/3_Memoization/FibonacciNumber.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Recursion_I._3_Memoization
+{
+    /// <summary>
+    /// https://leetcode.com/explore/learn/card/recursion-i/255/recursion-memoization/1661/
+    /// </summary>
+    public static class FibonacciNumber
+    {
+        public static int Fib(int n)
+        {
+            var cache = new Dictionary<int, int>();
+            return Next(n);
+
+            int Next(int idx)
+            {
+                switch (idx)
+                {
+                    case 0:
+                        cache[idx] = 0;
+                        break;
+                    case 1:
+                    case 2:
+                        cache[idx] = 1;
+                        break;
+                    default:
+                    {
+                        if (!cache.ContainsKey(idx))
+                        {
+                            var value = Next(idx - 1) + Next(idx - 2);
+                            cache[idx] = value;
+                        }
+                        
+                        break;
+                    }
+                }
+
+                return cache[idx];
+            }
+        }
+    }
+}
---
+++ TestSolutions/Recursion I/3_Memoization/FibonacciNumberTest.cs
@@ -0,0 +1,64 @@
+﻿using FluentAssertions;
+using LeetCode.Recursion_I._3_Memoization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestSolutions.Recursion_I._3_Memoization
+{
+    [TestClass]
+    public class FibonacciNumberTest
+    {
+        [TestMethod]
+        public void GetFibonacciNumberTestCase1()
+        {
+            FibonacciNumber.Fib(0).Should().Be(0);
+        }
+        
+        [TestMethod]
+        public void GetFibonacciNumberTestCase2()
+        {
+            FibonacciNumber.Fib(1).Should().Be(1);
+        }
+        
+        [TestMethod]
+        public void GetFibonacciNumberTestCase3()
+        {
+            FibonacciNumber.Fib(2).Should().Be(1);
+        }
+        
+        [TestMethod]
+        public void GetFibonacciNumberTestCase4()
+        {
+            FibonacciNumber.Fib(3).Should().Be(2);
+        }
+        
+        [TestMethod]
+        public void GetFibonacciNumberTestCase5()
+        {
+            FibonacciNumber.Fib(4).Should().Be(3);
+        }
+        
+        [TestMethod]
+        public void GetFibonacciNumberTestCase6()
+        {
+            FibonacciNumber.Fib(5).Should().Be(5);
+        }
+        
+        [TestMethod]
+        public void GetFibonacciNumberTestCase7()
+        {
+            FibonacciNumber.Fib(6).Should().Be(8);
+        }
+        
+        [TestMethod]
+        public void GetFibonacciNumberTestCase8()
+        {
+            FibonacciNumber.Fib(7).Should().Be(13);
+        }
+        
+        [TestMethod]
+        public void GetFibonacciNumberTestCase9()
+        {
+            FibonacciNumber.Fib(8).Should().Be(21);
+        }
+    }
+}
