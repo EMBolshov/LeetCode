06c34175d0e5ba67ea9f8f23ac49737af853899d
Evgeniy Bolshov <e.m.bolshov@gmail.com>
2021-04-05 21:26:22 +0300

    Recursion - Pow(x, n) implementation

---
+++ LeetCode/Recursion I/4_ComplexityAnalysis/Pow.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace LeetCode.Recursion_I._4_ComplexityAnalysis
+{
+    /// <summary>
+    /// https://leetcode.com/explore/learn/card/recursion-i/256/complexity-analysis/2380/
+    /// </summary>
+    public static class Pow
+    {
+        public static double MyPow(double x, int n)
+        {
+            const double minLimit = 0.00001;
+            const double maxLimit = double.MaxValue;
+            var result = GetPow(Math.Abs(x), n);
+            return x < 0 && n % 2 != 0 ? -result : result;
+
+            double GetPow(double x, int n)
+            {
+                if (n == 0)
+                    return 1;
+
+                if (x == 1)
+                    return 1;
+
+                if (n == int.MinValue)
+                    return 0;
+                
+                return n > 0 ? Multiply(x, n) : 1 / Multiply(x, Math.Abs(n));
+            }
+
+            double Multiply(double x, int n)
+            {
+                if (Math.Abs(x) <= minLimit)
+                    return 0;
+
+                if (Math.Abs(x) >= maxLimit - minLimit)
+                    return maxLimit;
+                
+                if (n == 0)
+                    return 1;
+
+                var res = x * Multiply(x, n - 1);
+                return res;
+            }
+        }
+    }
+}
---
+++ TestSolutions/Recursion I/4_ComplexityAnalysis/PowTest.cs
@@ -0,0 +1,94 @@
+﻿using FluentAssertions;
+using LeetCode.Recursion_I._4_ComplexityAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestSolutions.Recursion_I._4_ComplexityAnalysis
+{
+    [TestClass]
+    public class PowTest
+    {
+        [TestMethod]
+        public void MyPowTestCase1()
+        {
+            Pow.MyPow(2, 2).Should().Be(4);
+        }
+        
+        [TestMethod]
+        public void MyPowTestCase2()
+        {
+            Pow.MyPow(3, 2).Should().Be(9);
+        }
+        
+        [TestMethod]
+        public void MyPowTestCase3()
+        {
+            Pow.MyPow(2, 3).Should().Be(8);
+        }
+        
+        [TestMethod]
+        public void MyPowTestCase4()
+        {
+            Pow.MyPow(2, 0).Should().Be(1);
+        }
+        
+        [TestMethod]
+        public void MyPowTestCase5()
+        {
+            Pow.MyPow(2.5, 1).Should().Be(2.5);
+        }
+        
+        [TestMethod]
+        public void MyPowTestCase6()
+        {
+            Pow.MyPow(2.5, 2).Should().Be(6.25);
+        }
+        
+        [TestMethod]
+        public void MyPowTestCase7()
+        {
+            Pow.MyPow(2.00000, -2).Should().Be(0.25);
+        }
+        
+        [TestMethod]
+        public void MyPowTestCase8()
+        {
+            Pow.MyPow(-2, 2).Should().Be(4);
+        }
+        
+        [TestMethod]
+        public void MyPowTestCase9()
+        {
+            Pow.MyPow(-4, -2).Should().Be(0.0625);
+        }
+        
+        [TestMethod]
+        public void MyPowTestCase10()
+        {
+            Pow.MyPow(-2, 3).Should().Be(-8);
+        }
+        
+        [TestMethod]
+        public void MyPowTestCase11()
+        {
+            Pow.MyPow(-4, -3).Should().Be(-0.015625);
+        }
+        
+        [TestMethod]
+        public void MyPowTestCase12()
+        {
+            Pow.MyPow(0.00001, 2147483647).Should().Be(0);
+        }
+        
+        [TestMethod]
+        public void MyPowTestCase13()
+        {
+            Pow.MyPow(1, 2147483647).Should().Be(1);
+        }
+        
+        [TestMethod]
+        public void MyPowTestCase14()
+        {
+            Pow.MyPow(2.000000, -2147483648).Should().Be(0);
+        }
+    }
+}
