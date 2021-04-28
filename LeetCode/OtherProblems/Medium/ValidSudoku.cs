7d1116f2fa4a4d3a183a011760b1a2c08354cb32
Evgeniy Bolshov <e.m.bolshov@gmail.com>
2021-04-28 03:06:21 +0300

    Valid sudoku -- Parallel.For instead of regular for loop

--- LeetCode/OtherProblems/Medium/ValidSudoku.cs
+++ LeetCode/OtherProblems/Medium/ValidSudoku.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace LeetCode.OtherProblems.Medium
 {
@@ -42,51 +43,44 @@ namespace LeetCode.OtherProblems.Medium
                 new List<int>()
             };
 
-            for (int i = 0; i < 9; i++)
+            Parallel.For(0, 9, i =>
             {
-                if (board[i].Length != 9)
-                    return false;
-
                 var row = new List<int>();
-                
+
                 for (int j = 0; j < 9; j++)
                 {
                     if (int.TryParse(board[i][j].ToString(), out var n))
                     {
-                        if (n < 1 || n > 9)
-                            return false;
-
                         row.Add(n);
-                        
-                        
+
                         if (i < 3 && j < 3)
                             subBoxes[0].Add(n);
-                        
+
                         if (i < 3 && j >= 3 && j <= 5)
                             subBoxes[1].Add(n);
-                        
+
                         if (i < 3 && j > 5)
                             subBoxes[2].Add(n);
-                        
-                        
-                        
+
+
+
                         if (i >= 3 && i <= 5 && j < 3)
                             subBoxes[3].Add(n);
-                        
+
                         if (i >= 3 && i <= 5 && j >= 3 && j <= 5)
                             subBoxes[4].Add(n);
-                        
+
                         if (i >= 3 && i <= 5 && j > 5)
                             subBoxes[5].Add(n);
-                        
-                        
-                        
+
+
+
                         if (i > 5 && j < 3)
                             subBoxes[6].Add(n);
-                        
+
                         if (i > 5 && j >= 3 && j <= 5)
                             subBoxes[7].Add(n);
-                        
+
                         if (i > 5 && j > 5)
                             subBoxes[8].Add(n);
 
@@ -95,7 +89,7 @@ namespace LeetCode.OtherProblems.Medium
                 }
 
                 rows.Add(row);
-            }
+            });
 
 
             if (rows.Any(r => r.GroupBy(n => n).Any(n => n.Count() > 1)))
