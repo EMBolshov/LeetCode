2b16ae1bb7e84f748c51d9b773bcb52d61b989b8
Evgeniy Bolshov <e.m.bolshov@gmail.com>
2021-05-02 23:18:38 +0300

    Medium - Length of longest substring without repeating characters

---
+++ LeetCode/OtherProblems/Medium/LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.OtherProblems.Medium
+{
+    /// <summary>
+    /// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+    /// </summary>
+    public static class LongestSubstringWithoutRepeatingCharacters
+    {
+        public static int LengthOfLongestSubstring(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return 0;
+            if (s.Length == 1) return 1;
+
+            var result = 1;
+            var currentString = new HashSet<char>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                currentString.Add(s[i]);
+                
+                for (int j = i + 1; j < s.Length; j++)
+                {
+                    if (currentString.Contains(s[j]))
+                    {
+                        result = Complete();
+                        break;
+                    }
+
+                    currentString.Add(s[j]);
+
+                    if (j == s.Length - 1)
+                    {
+                        result = Complete();
+                        break;
+                    }
+                }
+            }
+
+            return result;
+
+            int Complete()
+            {
+                var count = currentString.Count;
+                if (count > result)
+                    result = count;
+
+                currentString.Clear();
+                return result;
+            }
+        }
+    }
+}
---
+++ TestSolutions/OtherProblems/Medium/LengthOfLongestSubstringTest.cs
@@ -0,0 +1,59 @@
+﻿using FluentAssertions;
+using LeetCode.OtherProblems.Medium;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestSolutions.OtherProblems.Medium
+{
+    [TestClass]
+    public class LengthOfLongestSubstringTest
+    {
+        [TestMethod]
+        public void TestCase1()
+        {
+            var input = "abcabcbb";
+            LongestSubstringWithoutRepeatingCharacters.LengthOfLongestSubstring(input).Should().Be(3);
+        }
+        
+        [TestMethod]
+        public void TestCase2()
+        {
+            var input = "bbbbbbb";
+            LongestSubstringWithoutRepeatingCharacters.LengthOfLongestSubstring(input).Should().Be(1);
+        }
+        
+        [TestMethod]
+        public void TestCase3()
+        {
+            var input = "pwwkew";
+            LongestSubstringWithoutRepeatingCharacters.LengthOfLongestSubstring(input).Should().Be(3);
+        }
+        
+        [TestMethod]
+        public void TestCase4()
+        {
+            var input = "";
+            LongestSubstringWithoutRepeatingCharacters.LengthOfLongestSubstring(input).Should().Be(0);
+        }
+        
+        [TestMethod]
+        public void TestCase5()
+        {
+            var input = " ";
+            LongestSubstringWithoutRepeatingCharacters.LengthOfLongestSubstring(input).Should().Be(1);
+        }
+        
+        [TestMethod]
+        public void TestCase6()
+        {
+            var input = "c";
+            LongestSubstringWithoutRepeatingCharacters.LengthOfLongestSubstring(input).Should().Be(1);
+        }
+        
+        [TestMethod]
+        public void TestCase7()
+        {
+            var input = "au";
+            LongestSubstringWithoutRepeatingCharacters.LengthOfLongestSubstring(input).Should().Be(2);
+        }
+    }
+}
