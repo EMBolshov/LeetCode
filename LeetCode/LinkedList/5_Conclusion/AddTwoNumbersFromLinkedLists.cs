192076978a0e81012ea90a9bbe1ac5bed2914505
Evgeniy Bolshov <e.m.bolshov@gmail.com>
2021-05-04 01:06:10 +0300

    Linked List - Conclusion - Add two numbers - version with dirty hack

---
+++ LeetCode/LinkedList/5_Conclusion/AddTwoNumbersFromLinkedLists.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using System.Numerics;
+using LeetCode.LinkedList._1_SinglyLinkedList;
+
+namespace LeetCode.LinkedList._5_Conclusion
+{
+    /// <summary>
+    /// https://leetcode.com/explore/learn/card/linked-list/213/conclusion/1228/
+    /// </summary>
+    public static class AddTwoNumbersFromLinkedLists
+    {
+        //BigInteger is a dirty hack for LeetCodes test cases with long Linked Lists. But right now I'm too lazy to make calculations by myself 
+        public static MySinglyLinkedList AddTwoNumbers(MySinglyLinkedList l1, MySinglyLinkedList l2)
+        {
+            var result = new MySinglyLinkedList();
+            var resCopy = result;
+            var firstNumber = string.Empty;
+            var secondNumber = string.Empty;
+
+            while (l1 != null)
+            {
+                firstNumber = l1.Value + firstNumber;
+                l1 = l1.Next;
+            }
+
+            while (l2 != null)
+            {
+                secondNumber = l2.Value + secondNumber;
+                l2 = l2.Next;
+            }
+
+            var resultNumber = (BigInteger.Parse(firstNumber) + BigInteger.Parse(secondNumber)).ToString().Reverse();
+
+            foreach (var n in resultNumber)
+            {
+                resCopy.Next = new MySinglyLinkedList(int.Parse(n.ToString()));
+                resCopy = resCopy.Next;
+            }
+
+            return result.Next;
+        }
+    }
+}
---
+++ TestSolutions/LinkedList/5_Conclusion/AddTwoNumbersFromLinkedListTest.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using FluentAssertions;
+using LeetCode.LinkedList._1_SinglyLinkedList;
+using LeetCode.LinkedList._5_Conclusion;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestSolutions.LinkedList.Common;
+
+namespace TestSolutions.LinkedList._5_Conclusion
+{
+    [TestClass]
+    public class AddTwoNumbersFromLinkedListTest
+    {
+        [TestMethod]
+        public void AddTwoNumbersTestCase1()
+        {
+            var list1 = CommonHelpers.GetLinkedListFromArray(new[] {2, 4, 3});
+            var list2 = CommonHelpers.GetLinkedListFromArray(new[] {5, 6, 4});
+            var output = AddTwoNumbersFromLinkedLists.AddTwoNumbers(list1, list2).ValuesToFlatList();
+            output.SequenceEqual(new[] {7, 0, 8}).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void AddTwoNumbersTestCase2()
+        {
+            var list1 = CommonHelpers.GetLinkedListFromArray(new[] {0});
+            var list2 = CommonHelpers.GetLinkedListFromArray(new[] {0});
+            var output = AddTwoNumbersFromLinkedLists.AddTwoNumbers(list1, list2).ValuesToFlatList();
+            output.SequenceEqual(new[] {0}).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void AddTwoNumbersTestCase3()
+        {
+            var list1 = CommonHelpers.GetLinkedListFromArray(new[] {9, 9, 9, 9, 9, 9, 9});
+            var list2 = CommonHelpers.GetLinkedListFromArray(new[] {9, 9, 9, 9});
+            var output = AddTwoNumbersFromLinkedLists.AddTwoNumbers(list1, list2).ValuesToFlatList();
+            output.SequenceEqual(new[] {8, 9, 9, 9, 0, 0, 0, 1}).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void AddTwoNumbersTestCase4()
+        {
+            var list1 = CommonHelpers.GetLinkedListFromArray(new[]
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1});
+            var list2 = CommonHelpers.GetLinkedListFromArray(new[] {5, 6, 4});
+            var output = AddTwoNumbersFromLinkedLists.AddTwoNumbers(list1, list2).ValuesToFlatList();
+            output.SequenceEqual(new[]
+                    {6, 6, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1})
+                .Should().BeTrue();
+        }
+    }
+}
