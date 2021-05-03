57f17f57624e6396c42549ddb2b6a7cd890ad43b
Evgeniy Bolshov <e.m.bolshov@gmail.com>
2021-05-04 00:30:24 +0300

    Linked List - Conclusion - Merge two sorted linked lists

---
+++ LeetCode/LinkedList/5_Conclusion/MergeTwoSortedLinkedLists.cs
@@ -0,0 +1,60 @@
+﻿using LeetCode.LinkedList._1_SinglyLinkedList;
+
+namespace LeetCode.LinkedList._5_Conclusion
+{
+    /// <summary>
+    /// https://leetcode.com/explore/learn/card/linked-list/213/conclusion/1227/
+    /// </summary>
+    public static class MergeTwoSortedLinkedLists
+    {
+        public static MySinglyLinkedList MergeTwoLists(MySinglyLinkedList l1, MySinglyLinkedList l2)
+        {
+            if (l1 == null && l2 == null) return null;
+            if (l1 == null) return l2;
+            if (l2 == null) return l1;
+
+            MySinglyLinkedList head;
+
+            if (l1.Value > l2.Value)
+            {
+                head = l2;
+                l2 = l2.Next;
+            }
+            else
+            {
+                head = l1;
+                l1 = l1.Next;
+            }
+            
+            var result = head;
+
+            while (head != null)
+            {
+                if (l1?.Value > l2?.Value)
+                {
+                    head.Next = l2;
+                    l2 = l2?.Next;
+                }
+                else if (l1 != null && l2 != null)
+                {
+                    head.Next = l1;
+                    l1 = l1.Next; 
+                }
+                else if (l1 == null)
+                {
+                    head.Next = l2;
+                    break;
+                }
+                else if (l2 == null)
+                {
+                    head.Next = l1;
+                    break;
+                }
+
+                head = head.Next;
+            }
+            
+            return result;
+        }
+    }
+}
---
+++ TestSolutions/LinkedList/5_Conclusion/MergeTwoSortedLinkedListsTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using FluentAssertions;
+using LeetCode.LinkedList._1_SinglyLinkedList;
+using LeetCode.LinkedList._5_Conclusion;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestSolutions.LinkedList.Common;
+
+namespace TestSolutions.LinkedList._5_Conclusion
+{
+    [TestClass]
+    public class MergeTwoSortedLinkedListsTest
+    {
+        [TestMethod]
+        public void MergeTwoListsTestCase1()
+        {
+            var list1 = CommonHelpers.GetLinkedListFromArray(new[] {1, 2, 4});
+            var list2 = CommonHelpers.GetLinkedListFromArray(new[] {1, 3, 4});
+            var output = MergeTwoSortedLinkedLists.MergeTwoLists(list1, list2).ValuesToFlatList();
+            output.SequenceEqual(new[] {1, 1, 2, 3, 4, 4}).Should().BeTrue();
+        }
+        
+        [TestMethod]
+        public void MergeTwoListsTestCase2()
+        {
+            var output = MergeTwoSortedLinkedLists.MergeTwoLists(null, null);
+            output.Should().BeNull();
+        }
+        
+        [TestMethod]
+        public void MergeTwoListsTestCase3()
+        {
+            var list2 = CommonHelpers.GetLinkedListFromArray(new[] {0});
+            var output = MergeTwoSortedLinkedLists.MergeTwoLists(null, list2).ValuesToFlatList();
+            output.SequenceEqual(new [] {0}).Should().BeTrue();
+        }
+        
+        [TestMethod]
+        public void MergeTwoListsTestCase4()
+        {
+            var list1 = CommonHelpers.GetLinkedListFromArray(new[] {1, 2, 4, 5, 6, 7});
+            var list2 = CommonHelpers.GetLinkedListFromArray(new[] {1, 3, 4});
+            var output = MergeTwoSortedLinkedLists.MergeTwoLists(list1, list2).ValuesToFlatList();
+            output.SequenceEqual(new[] {1, 1, 2, 3, 4, 4, 5, 6, 7}).Should().BeTrue();
+        }
+    }
+}
