e73efd7b84a3e9fd60a50dc24aa656318c168648
Evgeniy Bolshov <e.m.bolshov@gmail.com>
2021-05-04 01:30:19 +0300

    Arrays101 - Conclusion - Height checker

---
+++ LeetCode/Arrays101/6_Conclusion/HeightChecker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode.Arrays101._6_Conclusion
+{
+    /// <summary>
+    /// https://leetcode.com/explore/featured/card/fun-with-arrays/523/conclusion/3228/
+    /// </summary>
+    public static class HeightChecker
+    {
+        public static int NotMatchingIndices(int[] heights)
+        {
+            var sorted = new int[heights.Length];
+            Array.Copy(heights, sorted, heights.Length);
+            Array.Sort(sorted);
+
+            return heights.Where((t, i) => t != sorted[i]).Count();
+        }
+    }
+}
---
+++ TestSolutions/Array101/6_Conclusion/HeightCheckerTest.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using LeetCode.Arrays101._6_Conclusion;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestSolutions.Array101._6_Conclusion
+{
+    [TestClass]
+    public class HeightCheckerTest
+    {
+        [TestMethod]
+        public void NotMatchingIndicesTestCase1()
+        {
+            var input = new[] {1, 1, 4, 2, 1, 3};
+            HeightChecker.NotMatchingIndices(input).Should().Be(3);
+        }
+        
+        [TestMethod]
+        public void NotMatchingIndicesTestCase2()
+        {
+            var input = new[] {5, 1, 2, 3, 4};
+            HeightChecker.NotMatchingIndices(input).Should().Be(5);
+        }
+        
+        [TestMethod]
+        public void NotMatchingIndicesTestCase3()
+        {
+            var input = new[] {1, 2, 3, 4, 5};
+            HeightChecker.NotMatchingIndices(input).Should().Be(0);
+        }
+    }
+}
